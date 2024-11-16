ce8d92e643325a48a982cf102acd5b3c9ebb718d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-16 13:12:58 +0200

    Moved the folder to htdocs to handle server side

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<LoginPage />}/>
       <Route path='/home' element={<HomePage />}/>
+      <Route path='/*' element={<h1>Not Found</h1>}/>
     </Routes>
     </BrowserRouter>
     </>
--- app/src/pages/LoginPage.jsx
+++ app/src/pages/LoginPage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useNavigate} from "react-router-dom"
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("test@test.com");
+    const [password, setPassword] = useState(1234);
   return (
     <div>
+        <div>
+            <div>
+                <form>
+                    <input
+                    placeholder='Email Here'
+                    onChange={(event) => setEmail(event.target.value)}
+                    value={email}
+                    />
+                    <input
+                    placeholder='Password Here'
+                    onChange={(event) => setPassword(event.target.value)}
+                    value={password}
+                    />
+                    <button type="button">
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
         <button onClick={() => {
             navigate("/home");
         }}>Go to users</button>
