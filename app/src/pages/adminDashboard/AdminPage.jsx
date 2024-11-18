a192168e073034e3cd463c78d37b08e5d88f5716
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-18 22:01:55 +0200

    Created a Simple adminDashboard

--- app/src/pages/adminDashboard/AdminPage.jsx
+++ app/src/pages/adminDashboard/AdminPage.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import AdminNav from '../../components/AdminNav/AdminNav';
+
 
 const AdminPage = () => {
   return (
     <div>
-      <h2>Welcome to the Admin Dashboard</h2>
-      <p>Select a section to manage students, instructors, or courses.</p>
+        <AdminNav />
+      <div className="admin-content">
+        <h2>Welcome to the Admin Dashboard</h2>
+        {/* Add other admin page content here */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
